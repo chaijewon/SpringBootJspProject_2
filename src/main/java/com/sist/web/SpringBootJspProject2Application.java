9c19c9f09c72c8f1ca13e12b463a8d857db77479
chaijewon <vcandjava@nate.com>
2025-12-05 16:28:07 +0900

    1111

--- src/main/java/com/sist/web/SpringBootJspProject2Application.java
+++ src/main/java/com/sist/web/SpringBootJspProject2Application.java
@@ -2,7 +2,22 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ *   해당 폴더로 이동 
+ *   git init
+ *   git add .
+ *   git remote add origin git주소 
+ *   git branch -M main
+ *   git commit -m "1111"
+ *   git push -u origin main --force 
+ *   
+ *   --------------------------------
+ *   git add .
+ *   git commit -m "2222"
+ *   git push -u origin main
+ *   
+ *   
+ */
 @SpringBootApplication
 public class SpringBootJspProject2Application {
 
